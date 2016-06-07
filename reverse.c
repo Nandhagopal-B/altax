02d7895ae3d3c70d00077a70c76a1f8cf51f235e
Nandhagopal-B <nandhag101@gmail.com>
2016-06-07 14:43:24 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i,j,l;
+  char a[20],b[20];
+  printf("Enter a string:");
+  scanf("%s",&str);
+  for(i=0;a[i]!='\0';i++)
+  {
+    l++;
+  }
+  j=0;
+  for(i=l;i!=-1;i--)
+  {
+    b[j]=a[i];
+    j++;
+  }
+  printf("Reversed string:",b);
+  getch();
+}
